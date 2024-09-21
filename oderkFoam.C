72c8dc40f49b366c611c51d5f0a01d09a60269c4
demirmvc <hakan7demir@gmail.com>
2024-09-21 22:24:41 +0200

    last version of Guo

--- oderkFoam.C
+++ oderkFoam.C
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 
     Info<< "\nStarting time loop\n" << endl;
 
+
     dimensionedScalar b("b", dimensionSet(0, 1, -1, 0, 0, 0, 0), 1.0);
     double a = 1.0;
     double omega = 10.0;
@@ -30,10 +31,10 @@ int main(int argc, char *argv[])
         scalar t = runTime.value(); // current time 
         scalar dt = runTime.deltaT().value(); // time step size
 
-        volVectorField k1 = dt*(-a*U + b*vector(Foam::cos(omega*t), 0, 0));
-        volVectorField k2 = dt*(-a*(U + 0.5*k1) + b*vector(Foam::cos(omega*(t + 0.5*dt)), 0, 0));
-        volVectorField k3 = dt*(-a*(U + 0.5*k2) + b*vector(Foam::cos(omega*(t + 0.5*dt)), 0, 0));
-        volVectorField k4 = dt*(-a*(U + k3) + b*vector(Foam::cos(omega*(t + dt)), 0, 0));
+        volVectorField k1 = dt*(-a*U + b*vector(Foam::cos(omega*t), Foam::cos(omega*t),Foam::cos(omega*t)));
+        volVectorField k2 = dt*(-a*(U + 0.5*k1) + b*vector(Foam::cos(omega*(t + 0.5*dt)), Foam::cos(omega*(t + 0.5*dt)),Foam::cos(omega*(t + 0.5*dt))));
+        volVectorField k3 = dt*(-a*(U + 0.5*k2) + b*vector(Foam::cos(omega*(t + 0.5*dt)), Foam::cos(omega*(t + 0.5*dt)), Foam::cos(omega*(t + 0.5*dt))));
+        volVectorField k4 = dt*(-a*(U + k3) + b*vector(Foam::cos(omega*(t + dt)), Foam::cos(omega*(t + dt)),Foam::cos(omega*(t + dt))));
 
         U = U + (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4);
 
@@ -41,6 +42,7 @@ int main(int argc, char *argv[])
 
         runTime.write();
     }
+
 
     Info<< "End\n" << endl;
 
